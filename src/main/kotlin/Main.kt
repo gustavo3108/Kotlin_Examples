94720c9244c18dcc8a11a155713254be6ab7ee3e
Gustavo <gustavo-3108@hotmail.com>
2022-12-02 21:11:29 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package org.kotlinlang.play
+
+fun main() {        // Função Main sem frescura
+    println("Hello World!") // ;;;; não precisa ;;;;
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ")
+}
