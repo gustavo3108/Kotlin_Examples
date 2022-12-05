b503c1663c00b9d1fe932a5508a1b388f988ceac
Gustavo <gustavo-3108@hotmail.com>
2022-12-05 18:49:36 -0300

    Atualizando

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,30 +1,41 @@
 package org.kotlinlang.play
 
 
-    fun printMessage(message:String): Unit{
-        println(message)
-    }
-    fun printMessageWithPrefix(message:String, prefix: String = "Info"){
-        println("[$prefix] $message")
-    }
-    fun sum (x: Int, y: Int): Int{
-        return x + y;
-    }
-    fun multiply (x: Int, y: Int) = x * y
+//    fun printMessage(message:String): Unit{
+//        println(message)
+//    }
+//    fun printMessageWithPrefix(message:String, prefix: String = "Info"){
+//        println("[$prefix] $message")
+//    }
+//    fun sum (x: Int, y: Int): Int{
+//        return x + y;
+//    }
+//    fun multiply (x: Int, y: Int) = x * y
 
 fun main(){
-    printMessage("Hello")
-    printMessageWithPrefix("Hello")
-    printMessageWithPrefix(prefix="Log",message="Hello")
-    println(sum(2,5))
-    println(multiply(2,5))
-    var a ="Initial"
-    println(a)
-    a="Final"
-    println(a)
-    val b: Int=1 // val é imutável, não pode se atribuir outro valor
-    val c=3
-    println(b)
+    var neverNull: String = "This can't be bull"
+    //neverNull=null  //Não pode ser nula
+    var nullable: String? = "This can keep null here"// A ? permite a nulidade
+   nullable=null
+var inferredNonNull = "The compiler assumes non-null"//Var inferida não pode ser nula
+    //inferredNonNull=null
+    fun strLength(str: String?): Int {
 
-    println(c)
-}
+        return str?.length ?:0
+    }
+    println( "exemplo 1: " + strLength(neverNull))
+    println( "exemplo 2: " + strLength(nullable))
+//    printMessage("Hello")
+//    printMessageWithPrefix("Hello")
+//    printMessageWithPrefix(prefix="Log",message="Hello")
+//    println(sum(2,5))
+//    println(multiply(2,5))
+//    var a ="Initial"
+//    println(a)
+//    a="Final"
+//    println(a)
+//    val b: Int=1 // val é imutável, não pode se atribuir outro valor
+//    val c=3
+//    println(b)
+//    println(c)
+    }
