433846192c0a10e3d0d3641b19714b99a1f3484d
Gustavo <gustavo-3108@hotmail.com>
2022-12-06 19:52:06 -0300

    Atualizando

--- src/main/kotlin/TopicosAvancados.kt
+++ src/main/kotlin/TopicosAvancados.kt
@@ -33,21 +33,70 @@ package org.kotlinlang.play
 //
 //}
 //
-class Animal(val name: String)
-
-class Zoo(val animals: List<Animal>) {
-
-    operator fun iterator(): Iterator<Animal> {             // 1
-        return animals.iterator()                           // 2
-    }
-}
-
+//
+//fun main() {
+//
+//    for(i in 0..3) {             // 1
+//        print(i)
+//    }
+//    print(" ")
+//
+//    for(i in 0 until 3) {        // 2
+//        print(i)
+//    }
+//    print(" ")
+//
+//    for(i in 2..8 step 2) {      // 3
+//        print(i)
+//    }
+//    print(" ")
+//
+//    for (i in 3 downTo 0) {      // 4
+//        print(i)
+//    }
+//    print(" ")
+//}
+//fun main() {
+//
+//    for (c in 'a'..'d') {        // 1
+//        print(c)
+//    }
+//    print(" ")
+//
+//    for (c in 'z' downTo 'k' step 3) { // 2
+//        print(c)
+//    }
+//    print(" ")
+//    println()
+//    val x = 2
+//    if (x in 1..5) {            // 1
+//        print("x is in range from 1 to 5")
+//    }
+//    println()
+//
+//    if (x !in 6..10) {          // 2
+//        print("x is not in range from 6 to 10")
+//    }
+//}
+//Kotlin uses == for structural comparison and === for referential comparison.
+//More precisely, a == b compiles down to if (a == null) b == null else a.equals(b).
+//fun main() {
+//
+//
+//    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+//    val writers = authors//setOf("Twain", "Shakespeare", "Hemingway")
+//
+//    println(authors == writers)   // 1
+//    println(authors === writers)  // 2
+//
+//}
+//There is no ternary operator condition ? then : else in Kotlin. Instead, if may be used as an expression:
 fun main() {
 
-    val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
+    fun max(a: Int, b: Int) = if (a > b) a else b         // 1
 
-    for (animal in zoo) {                                   // 3
-        println("Watch out, it's a ${animal.name}")
-    }
+    println(max(-99, -42))
 
 }
+
+
